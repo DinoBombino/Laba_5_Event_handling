72acc3cee6472766530550043b47beebf4df0d1d
DinoBombino <137479606+DinoBombino@users.noreply.github.com>
2024-04-28 21:22:41 +0800

    Сделал методичку

---
+++ Laba_5_Event_handling/Form1.cs
@@ -0,0 +1,114 @@
+using Laba_5_Event_handling.Objects;
+
+namespace Laba_5_Event_handling
+{
+    public partial class Form1 : Form
+    {
+        //MyRectangle myRect;
+        List<BaseObject> objects = new();
+        Player player;
+        Marker marker;
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+
+            player.OnOverlap += (p, obj) => //reakcia na peresechenie
+            {
+                txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] Игрок пересекся с {obj}\n" + txtLog.Text;
+            };
+
+            player.OnMarkerOverlap += (m) => //reactia na peresechenie s markerom
+            {
+                objects.Remove(m);
+                marker = null;
+            };
+
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+            objects.Add(marker);
+            objects.Add(player);
+
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
+
+            //myRect = new MyRectangle(100, 100, 45); 
+        }
+
+        private void pbMain_paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.White);
+
+            updatePlayer();
+
+            // пересчитываем пересечения
+            foreach (var obj in objects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                    obj.Overlap(player);
+                }
+            }
+
+            // рендерим объекты
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+        }
+
+        public void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+                float length = MathF.Sqrt(dx * dx + dy * dy);
+                dx /= length;
+                dy /= length;
+
+                // по сути мы теперь используем вектор dx, dy
+                // как вектор ускорения, точнее даже вектор притяжения
+                // который притягивает игрока к маркеру
+                // 0.5 просто коэффициент который подобрал на глаз
+                // и который дает естественное ощущение движения
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+
+                // расчитываем угол поворота игрока 
+                player.Angle = 90 - MathF.Atan2(player.vX, player.vY) * 180 / MathF.PI;
+            }
+
+            // тормозящий момент,
+            // нужен чтобы, когда игрок достигнет маркера произошло постепенное замедление
+            player.vX += -player.vX * 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            // пересчет позиция игрока с помощью вектора скорости
+            player.X += player.vX;
+            player.Y += player.vY;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //updatePlayer();
+
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                objects.Add(marker);
+            }
+
+            marker.X = e.X;
+            marker.Y = e.Y;
+        }
+    }
+}
---
+++ Laba_5_Event_handling/Objects/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_5_Event_handling.Objects
+{
+    class Player : BaseObject
+    {
+        public float vX, vY;
+        public Action<Marker> OnMarkerOverlap;
+        public Player(float x, float y, float angle) : base(x, y, angle) //konstruktor
+        {
+        }
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.DeepSkyBlue), -15, -15, 30, 30);//kruzhochek
+            g.DrawEllipse(new Pen(Color.Black, 2), -15, -15, 30, 30);//ramka dlya kruzhochka
+            g.DrawLine(new Pen(Color.Black, 2), 0, 0, 25, 0);//palka napravleniya
+        }
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-15, -15, 30, 30);
+            return path;
+        }
+        public override void Overlap(BaseObject obj)
+        {
+            base.Overlap(obj);
+
+            if (obj is Marker)
+            {
+                OnMarkerOverlap(obj as Marker);
+            }
+        }
+    }
+}
